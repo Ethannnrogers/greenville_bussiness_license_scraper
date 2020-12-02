984cd2919b0b1ce6b6c2d9354dd3dddc518ab0af
aaron-schuyler <asky28@gmail.com>
2020-12-02 16:14:33 -0500

    fixed typo errors

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -5,18 +5,18 @@ class DataController < ApplicationController
     IO.copy_stream(URI.open("https://www.greenvillesc.gov/DocumentCenter/View/16030/2020-Non-Resident-Business-License-Listing-XLS"), 'non-resident-file.xlsx')
     non_resident_data = Roo::Spreadsheet.open('non-resident-file.xlsx')
     headers = resident_data.row(2)
-    resident_formatted = resident_data.drop(2).take(a.size - 1).map do |r|
+    resident_formatted = resident_data.drop(2).take(resident_data.size - 1).map do |r|
       row = r.map do |s|
         s.to_s.strip
       end
       Hash[headers.zip(row)]
     end
-    non_resident_formatted = non_resident_data.drop(2).take(a.size - 1).map do |r|
+    non_resident_formatted = non_resident_data.drop(2).take(non_resident_data.size - 1).map do |r|
       row = r.map do |s|
         s.to_s.strip
       end
       Hash[headers.zip(row)]
     end
-    render json: resident_formatted()
+    render json: resident_formatted.concat(non_resident_formatted)
   end
 end
