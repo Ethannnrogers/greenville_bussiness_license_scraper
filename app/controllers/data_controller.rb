33261084b321b91bf0b03674e89293dfa0d667ee
aaron-schuyler <asky28@gmail.com>
2020-12-02 16:26:42 -0500

    fixed |

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -2,10 +2,10 @@ class DataController < ApplicationController
   def index
     IO.copy_stream(URI.open("https://www.greenvillesc.gov/DocumentCenter/View/16030/2020-Resident-Business-License-Listing-XLS"), 'resident-file.xlsx')
     resident_data = Roo::Spreadsheet.open('resident-file.xlsx')
-    IO.copy_stream(URI.open("https://www.greenvillesc.gov/DocumentCenter/View/16030/2020-Non-Resident-Business-License-Listing-XLS"), 'non-resident-file.xlsx')
+    IO.copy_stream(URI.open("https://www.greenvillesc.gov/DocumentCenter/View/16029/2020-Non-Resident-Business-License-Listing-XLS"), 'non-resident-file.xlsx')
     non_resident_data = Roo::Spreadsheet.open('non-resident-file.xlsx')
     headers = resident_data.row(2)
-    resident_formatted = resident_data.drop(2).map do |r
+    resident_formatted = resident_data.drop(2).map do |r|
       row = r.map do |s|
         s.to_s.strip
       end
